abe9d59830338928a4353c934ed7c18ef2c999cb
TacoConKvass <e_frun@o2.pl>
2025-05-27 02:20:50 +0200

    [Add] Fish base file

---
+++ src/TacosSeaCreatures/Content/NPCs/Fish.cs
@@ -0,0 +1,31 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace TacosSeaCreatures;
+
+public class Fish : ModNPC {
+	public override void SetDefaults() {
+		NPC.Size = new Vector2(20, 30);
+
+		NPC.lifeMax = 40;
+		NPC.damage = 4;
+
+		NPC.noGravity = true;
+	}
+
+	public Rectangle VariantRect;
+	public const int VariantCount = 0;
+
+	public override void OnSpawn(IEntitySource source) {
+		int variant = Main.rand.Next(0, VariantCount);
+		VariantRect = new Rectangle(variant * NPC.width, 0, NPC.width, NPC.height);
+	}
+
+	public override void AI() { }
+
+	public override void FindFrame(int frameHeight) {
+		NPC.frame = VariantRect;
+	}
+}
