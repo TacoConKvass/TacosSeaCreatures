1e73ba24c3944568b32964ff1a9cce6165abc277
TacoConKvass <e_frun@o2.pl>
2025-05-27 02:03:15 +0200

    [Add] SeahorseAdult base

---
+++ src/TacosSeaCreatures/Content/NPCs/SeahorseAdult.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+
+namespace TacosSeaCreatures.NPCs;
+
+public class SeahorseAdult : ModNPC {
+	public override void SetDefaults() {
+		NPC.Size = new Vector2(20, 30);
+
+		NPC.lifeMax = 40;
+		NPC.damage = 4;
+
+		NPC.noGravity = true;
+	}
+
+	public SeahorseAction State = SeahorseAction.Idle;
+
+	public override void AI() {
+		switch (State) {
+			case SeahorseAction.Idle:
+				Idle();
+				break;
+			case SeahorseAction.Chase:
+				Chase();
+				break;
+			case SeahorseAction.Shoot:
+				Shoot();
+				break;
+		}
+	}
+
+	public void Idle() { }
+
+	public void Chase() { }
+
+	public void Shoot() { }
+
+	public override void FindFrame(int frameHeight) { }
+}
+
+public enum SeahorseAction {
+	Idle,
+	Chase,
+	Shoot,
+}
