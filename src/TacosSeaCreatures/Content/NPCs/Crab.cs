c83ed0cd95e7bf013a5f561911b5e84dec7f1e6e
TacoConKvass <e_frun@o2.pl>
2025-05-28 22:19:32 +0200

    [Add ] Consts for Crab behaviour

--- src/TacosSeaCreatures/Content/NPCs/Crab.cs
+++ src/TacosSeaCreatures/Content/NPCs/Crab.cs
@@ -20,6 +20,12 @@ public class Crab : ModNPC {
 
 	public Direction Movement = Direction.Left;
 
+	public static readonly Vector2 WallCheck = Vector2.UnitX * 1.5f * Consts.TILE_SIZE;
+	public const float MOVE_SPEED = 1.3f;
+	public const float MIN_TURNAROUND_MULTIPLIER = 1.5f;
+	public const float MAX_TURNAROUND_MULTIPLIER = 1.5f;
+	public const float BASE_TURNAROUND_TIEMR = 360;
+
 	public ref float Timer => ref NPC.ai[0];
 
 	public float RNG {
@@ -31,20 +37,19 @@ public class Crab : ModNPC {
 	}
 
 	public override void OnSpawn(IEntitySource source) {
-		RNG = Main.rand.NextFloat(1.5f, 3f);
+		RNG = Main.rand.NextFloat(MIN_TURNAROUND_MULTIPLIER, MAX_TURNAROUND_MULTIPLIER);
 	}
 
 	public override void AI() {
 		Timer++;
-		if (((int)Timer % (int)(360 * RNG)) == 0 || Main.tile[(NPC.Center + Vector2.UnitX * 1.5f * Consts.TILE_SIZE * (int)Movement).ToTileCoordinates()].HasTile ) {
+		if (((int)Timer % (int)(BASE_TURNAROUND_TIEMR * RNG)) == 0 || Main.tile[(NPC.Center + WallCheck * (int)Movement).ToTileCoordinates()].HasTile ) {
 			Timer = 0;
-			RNG = Main.rand.NextFloat(1.5f, 5f);
+			RNG = Main.rand.NextFloat(MIN_TURNAROUND_MULTIPLIER, MAX_TURNAROUND_MULTIPLIER);
 			Movement = Movement.Reversed();
 		}
 
-		NPC.velocity = new Vector2((int)Movement * 1.3f, 0);
-		if (NPC.wet) NPC.GravityMultiplier *= 30f;
-		Collision.FindCollisionDirection(out int collisionDirection, NPC.Center, NPC.width, NPC.height);
+		NPC.velocity = new Vector2((int)Movement * MOVE_SPEED, 0);
+		if (NPC.wet) NPC.GravityMultiplier *= 25f;
 		Collision.StepUp(ref NPC.position, ref NPC.velocity, NPC.width, NPC.height, ref NPC.stepSpeed, ref NPC.gfxOffY);
 	}
 
