c39ba94c396a06f777a3cb2d70fd1cf5d9d88258
TacoConKvass <e_frun@o2.pl>
2025-06-10 10:49:44 +0200

    [Update] Bubble animation

--- src/TacosSeaCreatures/Common/GlobalSlime.cs
+++ src/TacosSeaCreatures/Common/GlobalSlime.cs
@@ -34,9 +34,11 @@ public class GlobalSlime : GlobalNPC {
 
 	public override bool PreAI(NPC npc) {
 		if (InABubble) {
+			if (npc.ai[0] < 0) npc.ai[0] = 0;
+			
 			npc.ai[0]++;
 			
-			npc.velocity = Main.tile[npc.Center.ToTileCoordinates() + new Point(0, 2)].LiquidAmount > 0 ? -3 * Vector2.UnitY : BobbingVector * MathF.Cos(npc.ai[0] / MathHelper.TwoPi / 3);
+			npc.velocity = Main.tile[npc.Center.ToTileCoordinates() + new Point(0, 3)].LiquidAmount > 0 ? -3 * Vector2.UnitY : BobbingVector * MathF.Cos(npc.ai[0] / MathHelper.TwoPi / 3);
 			npc.target = npc.FindClosestPlayer();
 			if (npc.HasValidTarget) {
 				npc.velocity.X = npc.Center.DirectionTo(Main.player[npc.target].Center).X > 0 ? .5f : -.5f;
@@ -54,8 +56,12 @@ public class GlobalSlime : GlobalNPC {
 
 	public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
 		if (InABubble) {
-			Rectangle sourceRectangle = new Rectangle(0, 0, BubbleTexture.Value.Width, BubbleTexture.Value.Height);
-			Main.EntitySpriteDraw(BubbleTexture.Value, npc.Center - screenPos, sourceRectangle, drawColor, 0, sourceRectangle.Center(), npc.scale, SpriteEffects.None);
+			int frameHeight = (BubbleTexture.Value.Height / 6);
+			int width = BubbleTexture.Value.Width;
+			Main.NewText(npc.ai[0]);
+			//Main.NewText((int)(((int)npc.ai[0] % 120) / 20) * frameHeight);
+			Rectangle sourceRectangle = new Rectangle(0, (int)(((int)npc.ai[0] % 120) / 20) * frameHeight, width, frameHeight - 1);
+			Main.EntitySpriteDraw(BubbleTexture.Value, npc.Center - screenPos, sourceRectangle, drawColor, 0, new Vector2(width /2, frameHeight / 2), npc.scale, SpriteEffects.None);
 		}
 		
 		return true;
